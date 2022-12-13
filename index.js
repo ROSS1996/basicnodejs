e8234014c2c7adce5dbb7b940d94e51fb61cf7a1
Robson <41270745+ROSS1996@users.noreply.github.com>
2022-12-13 16:03:43 -0300

    Change router to express

--- index.js
+++ index.js
@@ -1,48 +1,28 @@
-const http = require('http');
-const fs = require('fs')
-
-const hostname = '127.0.0.1'
+const path = require('path');
+const express = require("express")
 const port = 3000
 
-const server = http.createServer((req,res)=>{
-    if(req.url==='/' || req.url==='/index' || req.url==='/index.html'){
-        fs.readFile('./index.html',function(err,data){
-            if (err) throw err;
-            res.writeHead(200,{'Content-Type':'text/html'});
-            res.write(data);
-            return res.end();
-        });
-    }
-    else if(req.url==='/about' || req.url==='/about.html'){
-        fs.readFile('./about.html',function(err,data){
-            if (err) throw err;
-            res.writeHead(200,{'Content-Type':'text/html'});
-            res.write(data);
-            return res.end();
-        });
-    }
-    else if(req.url==='/contact' || req.url==='/contact.html'){
-        fs.readFile('./contact.html',function(err,data){
-            if (err) throw err;
-            res.writeHead(200,{'Content-Type':'text/html'});
-            res.write(data);
-            return res.end();
-        });
-    }
-    else if (req.url == '/style.css') {
-        res.setHeader('Content-type', 'text/css');
-        res.write(fs.readFileSync('style.css'));
-        return res.end();
-    } else {
-        fs.readFile('./404.html',function(err,data){
-            if (err) throw err;
-            res.writeHead(200,{'Content-Type':'text/html'});
-            res.write(data);
-            return res.end();
-        });
-    }
-})
+const server = express()
 
-server.listen(port, hostname, () => {
-    console.log(`Server running at http://${hostname}:${port}/`)
+server.use(express.static("public"))
+
+server.get('/', (req, res) => {
+    res.sendFile(path.resolve(__dirname, 'public/', 'index.html'));
+});
+
+server.get('/about', (req, res) => {
+    res.sendFile(path.resolve(__dirname, 'public/', 'about.html'));
+});
+
+server.get('/contact', (req, res) => {
+    res.sendFile(path.resolve(__dirname, 'public/', 'contact.html'));
+});
+
+server.get('*', function(req, res){
+    res.status(404).sendFile(path.resolve(__dirname, 'public/', '404.html'));
+  });
+
+
+server.listen(port, () => {
+    console.log(`Server running at http://localhost:${port}/`)
   })
